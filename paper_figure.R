b750d3080475d43858e71ce920656502a3447f00
Andrea Bellavia <18037502+andreabellavia@users.noreply.github.com>
2022-02-16 14:46:54 -0500

    vignetteupdate

--- paper_figure.R
+++ paper_figure.R
@@ -1,6 +1,7 @@
 library(interactionRCS)
 library(survival)
 library(rms)
+library(readxl)
 sd_training7 <- read_excel("Z:/Minao/AHA Grant/AHA Abstract/data/sd_training7.xls")
 
 myformula <- Surv(days2miistr, miistrfu) ~ curr_smoke*rcs(age, 3)
@@ -18,3 +19,83 @@ HR_loglin_delta <- loglinHR( var2values = c(50:80)
                              , model = model_loglin , data = sd_training7 , var1 ="curr_smoke", var2="age", ci.method = "delta")
 
 plotHR(HR_loglin_delta , xlab = "Age")
+
+
+
+# with coxph, good
+model_loglin <- coxph(myformula , data = sd_training7 )
+
+HR_loglin_delta2 <- loglinHR( var2values = c(50:80)
+                             , model = model_loglin , data = sd_training7 , var1 ="curr_smoke", var2="age", ci.method = "delta")
+
+plotHR(HR_loglin_delta2 , xlab = "Age")
+
+
+###### logistic
+
+
+# rcs needs rms::lrm
+modellog <- rms::lrm( hxcad~ curr_smoke*rcs(age, 3),data = sd_training7)
+
+OR_rcs_delta <- rcsOR( var2values = c(50:80)
+                          , model = modellog , data = sd_training7 , var1 ="curr_smoke", var2="age" ,ci.method = "delta")
+
+plotINT(OR_rcs_delta , xlab = "Age")
+
+
+
+
+# log linear good with both
+modellog <- rms::lrm( hxcad~ curr_smoke*age,data = sd_training7)
+
+OR_loglin_delta <- loglinOR( var2values = c(50:80)
+                        , model = modellog , data = sd_training7 , var1 ="curr_smoke", var2="age" ,ci.method = "delta")
+
+plotINT(OR_loglin_delta , xlab = "Age")
+
+
+
+modellog <- glm( hxcad~ curr_smoke*age,data = sd_training7,family="binomial")
+
+
+
+OR_loglin_delta2 <- loglinOR( var2values = c(50:80)
+                          , model = modellog , data = sd_training7 , var1 ="curr_smoke", var2="age" ,ci.method = "delta")
+
+plotINT(OR_loglin_delta2 , xlab = "Age")
+
+
+
+
+#### linear model
+
+# rcs needs rms::Glm
+
+modellin <- rms::Glm( BLGFR~ curr_smoke*rcs(age, 3),data = sd_training7)
+
+
+
+lin_rcs_delta <- rcsLIN( var2values = c(50:80)
+                        , model = modellin , data = sd_training7 , var1 ="curr_smoke", var2="age" ,ci.method = "delta")
+
+plotINT(lin_rcs_delta , xlab = "Age")
+
+# linear good with the second one
+modellin <- rms::Glm( BLGFR~ curr_smoke*age,data = sd_training7)
+
+lin_delta <- linLIN( var2values = c(50:80)
+                        , model = modellin , data = sd_training7 , var1 ="curr_smoke", var2="age" ,ci.method = "delta")
+
+plotINT(lin_delta , xlab = "Age")
+
+#
+modellin <- glm( BLGFR~ curr_smoke*age,data = sd_training7)
+
+
+
+lin_delta2 <- linLIN( var2values = c(50:80)
+                        , model = modellin , data = sd_training7 , var1 ="curr_smoke", var2="age" ,ci.method = "delta")
+
+plotINT(lin_delta2 , xlab = "Age")
+
+
